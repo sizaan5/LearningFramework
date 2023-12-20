8f58124d4b3f005f82f54708d3d40926127f3c69
Izaan Saleem <izaan.saleem@m3tech.com.pk>
2023-12-20 15:32:34 +0500

    0.0.2 Added framework

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let package = Package(
+/*let package = Package(
     name: "LearningFramework",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
@@ -19,5 +19,29 @@ let package = Package(
         .testTarget(
             name: "LearningFrameworkTests",
             dependencies: ["LearningFramework"]),
+    ]
+)*/
+let package = Package(
+    name: "LearningFramework",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "LearningFramework",
+            targets: ["LearningFramework"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "LearningFramework",
+            path: "./Sources/LearningFramework.xcframework"
+        )
     ]
 )
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # LearningFramework
 
-0.0.1
+0.0.2
+
+- Created repo and push -   0.0.1
+- Added framework       -   0.0.2 
